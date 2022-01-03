21a70788e7beb6d5fd3e11c38a46844b5d53009e
Deepak Acharya <64803000+acdepak@users.noreply.github.com>
2022-01-03 14:01:37 +0545

    navigation panel added

---
+++ nav.php
@@ -0,0 +1,53 @@
+<!-- <html>
+    <head>
+        <title>navbar</title> -->
+        <style>
+            body {
+                margin: 0;
+            }
+            /* div.navbar {
+                background-color: blue;
+            } */
+            ul.topnav {
+                list-style-type: none;
+                margin: 0;
+                padding: 0;
+                overflow: hidden;
+                background-color: rgb(125, 44, 149);
+            }
+            ul.topnav div.lnav li {float: right;}
+            ul.topnav div.rnav li {float: left;}
+            ul.topnav li a{
+                display: block;
+                color: white;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none; /* removes all decoreations*/
+            }
+            /* ul.topnav li a:hover:not(.active) {background-color: #111;}
+
+            ul.topnav li a.active {background-color: #04AA6D;} */
+        </style>
+    <!-- <head>
+    <body> -->
+
+            <div class="a">
+                <div class="navbar">
+                    <ul class="topnav">
+                        <div class="lnav">
+                            <li><a href="#profile">Profile</a></li>
+                            <li><a href="#search">Search</a></li>
+                        </div>
+                        <div class="rnav">
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#category">Category</a></li>
+                            <li><a href="#invoice">Invoice</a></li>
+                            <li><a href="#bill">Bill</a></li>
+                        </div>
+                        
+                        
+                    </ul>
+            </div>
+        </div>
+       <!-- </body>
+</html> -->
