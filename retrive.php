99bbca153907e9d53d688a70a3438d70367402b3
Deepak Acharya <64803000+acdepak@users.noreply.github.com>
2022-03-31 13:18:38 +0545

    retrives data

---
+++ retrive.php
@@ -0,0 +1,50 @@
+<?php
+include_once 'session.php';
+include_once 'nav.php';
+
+$result = mysqli_query($mysqli,"SELECT * FROM category");
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title> Retrive data</title>
+    <link rel="stylesheet" href="#">
+  </head>
+  <body>
+    <?php
+    if (mysqli_num_rows($result) > 0) {
+    ?>
+    <table>
+      <tr>
+        <td>SN</td>
+        <td>Categories</td>
+        <td>Quantity</td>
+        <td>Unit</td>
+        <td>Rate</td>
+      </tr>
+            <?php
+            $i=0;
+            while($row = mysqli_fetch_array($result)) {
+            ?>
+      <tr>
+        <td><?php echo $row['SN']; ?></td>
+        <td><?php echo $row['Categories']; ?></td>
+        <td><?php echo $row['Quantity']; ?></td>
+        <td><?php echo $row['Unit']; ?></td>
+        <td><?php echo $row['Rate']; ?></td>
+      </tr>
+            <?php
+            $i++;
+            }
+            ?>
+    </table>
+    <?php
+    }
+    else
+      {
+          echo "No result found";
+      }
+    ?>
+  </body>
+</html>
