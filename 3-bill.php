c4eb11f43789ef6c3e91f3a28fd7e11d1a2e69d0
acdepak <acdepak@gmail.com>
2023-01-24 14:53:50 +0545

    final commit

--- 3-bill.php
+++ 3-bill.php
@@ -1,6 +1,7 @@
 <?php
     include_once 'session.php';
     include_once 'nav.php';
+    include_once 'bill_up.php';
 
 //declaring variables
     $price="";
@@ -10,17 +11,28 @@
     $Unit="";       $Rate="";
 
 // storing the values in the input field to render
-// if(isset($_GET['button'])) {
-//     $c_name=$_GET['c_name'];    $quantity=$_GET['qty'];
-//     $Categories=$_GET['Categories'];    $Unit=$_GET['Unit'];
-//     $Rate=$_GET['Rate'];    $u_name=$_GET['u_name'];
-// }
+if(isset($POST['btninsert'])) 
+{
+    $c_name=$_POST['c_name'];    $quantity=$_POST['qty'];
+    $Categories=$_POST['Categories'];    $Unit=$_POST['Unit'];
+    $Rate=$_POST['Rate'];    $u_name=$_POST['u_name'];
 
-// fetching from category table in database
-$result = mysqli_query($mysqli,"SELECT * FROM category
-WHERE Categories='".$_GET['Categories']."'");
-$row = mysqli_fetch_array($result);
-  
+    // $mysqli=mysqli_connect("localhost","root","","dairymgmtsystem");
+    // if(!$mysqli){
+    //     echo ("Error connecting to database");
+    // }
+    // else{
+        $sql = "INSERT INTO invoice (`SN`, `Date`, `Customer`, `Product`, `Quantity`, `Rate`, `Price`, `Username`) VALUES (NULL, '$date', '$c_name', '$Categories', '$quantity', '$Unit', '$Rate', '$price', '$u_name')"; 
+        
+        if(mysqli_query($mysqli,$sql))
+        { 
+            echo("Data Saved");}
+        else
+        {
+            echo("!!! DATA NOT Saved !!!");
+        }
+   // }
+} 
 ?>
 
 <!DOCTYPE html>
@@ -38,31 +50,32 @@ $row = mysqli_fetch_array($result);
             <h3>Billing</h3>
         </div>
             <div id="div8">
-                <form class='inputfield' action="bill_up.php" method="get">
-                    <label>Date :</label><input style="border:hidden;" disabled name="date" value="<?php echo $date; ?>"><br>
+                <form action="3-bill.php" class='inputfield' method="post">
+                    <label>Date :</label><input style="border:hidden;" type="text" name="date" value="<?php echo $date; ?>"><br>
                     <label>Customer name: </label>
                     <input syle=""type="text" name="c_name" value="<?php echo $c_name; ?>" placeholder="Name..."><br>
                     
                     <label>Product: </label>
-                    <input disabled name="Categories" id="Categories" value="<?php echo $row['Categories']; ?>">
+                    <!-- <input type="hidden" name="Categories" class="txtField""value="<?php echo $row['Categories']; ?>"> -->
+                    <input type="text" name="Categories" id="Categories" style="border:hidden;" value="<?php echo $row['Categories']; ?>">
                                                         
                     <br>
                     <label>Unit: </label>
-                    <input disabled name="Unit" id="Unit" value="<?php echo $row['Unit']; ?>">
+                    <input type="text" name="Unit" id="Unit" style="border:hidden;"value="<?php echo $row['Unit']; ?>">
                     
                     <br>
                     <label>Rate: </label>
-                    <input disabled name="Rate" id="Rate" value="<?php echo $row['Rate']; ?>">
+                    <input type="text" name="Rate" id="Rate" style="border:hidden;"value="<?php echo $row['Rate']; ?>">
                     
                     <br>
                     <label>Quantity: </label>
                     <input type="text" name="qty" value="<?php echo $quantity; ?>"
                     id="qty" onkeyup="myFunction()"><br>
                     <label>Price: </label>
-                    <input disabled name="price" id="price" value="<?php echo ($price); ?>"><br>
+                    <input type="text" name="price" id="price" style="border:hidden;"value="<?php echo ($price); ?>"><br>
                     <label>Username: </label>
                     <input syle=""type="text" name="u_name" value="<?php echo $u_name; ?>"><br>
-                    <input type="submit" name="button" value="Submit"><br>
+                    <input type="submit" name="btninsert" value="Save"><br>
                 </form>
             </div>
         </div>
@@ -71,7 +84,7 @@ $row = mysqli_fetch_array($result);
         function myFunction(){
             var rate = document.getElementById("Rate").value;
             var qty = document.getElementById("qty").value;
-            document.getElementById("price").value= "Rs." + (Number(rate)*Number(qty));
+            document.getElementById("price").value= (Number(rate)*Number(qty));
         }
     </script>    
 </body>
--- bill_up.php
+++ bill_up.php
@@ -1,26 +1,10 @@
 <?php
 //only php for inserting in invoice table
 include_once 'session.php';
-include_once 'nav.php';
 
-$price="";
-$date=date("Y-m-d");
-$c_name="";    $quantity="";    $u_name="Deepak";
-$Categories="";
-$Unit="";       $Rate="";
-
-if(count($_GET)>0) {
-// inserting in invoice
-$query = "INSERT INTO `invoice` (`SN`, `Date`, `Customer`, `Product`, `Quantity`, `Rate`, `Price`, `Username`) 
-VALUES (NULL, '$date', '$c_name', '$Categories', '$quantity', '$Unit', '$Rate', '$price', '$u_name');"; 
-    if(mysqli_query($mysqli,$query))
-    { 
-        echo("Data Saved");}
-    else
-    {
-        echo("!!! DATA NOT Saved !!!");
-    }
-}
+// fetching from category table in database
+$result = mysqli_query($mysqli,"SELECT * FROM category WHERE Categories='".$_GET['Categories']."'");
+$row = mysqli_fetch_array($result);
 
 
 ?>
